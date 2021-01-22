83e2d4a0998df4490ca9932b4b935f4c1ac11d8f
lesileqin <1722249371@qq.com>
2021-01-22 13:07:40 +0800

    master

--- MyBatis/MyBatisDemo3/MyBatis-01/src/main/java/com/wzq/mapper/UserMapper.java
+++ MyBatis/MyBatisDemo3/MyBatis-01/src/main/java/com/wzq/mapper/UserMapper.java
@@ -6,6 +6,19 @@ import java.util.List;
 
 public interface UserMapper {
 
+    //查询所有用户
     List<User> findAll();
+
+    //根据ID查询用户
+    User findUserById(int id);
+
+    //插入一个用户
+    void addUser(User user);
+
+    //修改用户
+    void updateUser(User user);
+
+    //删除用户
+    void deleteUser(int id);
 
 }
--- MyBatis/MyBatisDemo3/MyBatis-01/src/test/java/com/wzq/mapper/UserMapperTest.java
+++ MyBatis/MyBatisDemo3/MyBatis-01/src/test/java/com/wzq/mapper/UserMapperTest.java
@@ -8,21 +8,61 @@ import org.junit.Test;
 import java.util.List;
 
 public class UserMapperTest {
+
+    //抽取属性，获取SqlSession对象
+    private SqlSession sqlSession = MyBatisUtils.getSqlSession();
+    //抽取属性，获取mapper
+    private UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+    //测试查询所有用户
     @Test
     public void findAllTest(){
-
-        //1. 获取SqlSession
-        SqlSession sqlSession = MyBatisUtils.getSqlSession();
-
         //2. 执行
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         List<User> all = mapper.findAll();
         for (User user : all) {
             System.out.println(user);
         }
-
         //3. 关闭连接
         sqlSession.close();
-
     }
+
+    //测试根据ID查询用户
+    @Test
+    public void findUserByIdTest(){
+        //2. 执行
+        User user = mapper.findUserById(1);
+        System.out.println(user);
+        //3. 关闭连接
+        sqlSession.close();
+    }
+
+    //增删改需要提交事务
+    //插入一个用户
+    @Test
+    public void addUserTest(){
+        User user = new User(null,"大王","1234215");
+        mapper.addUser(user);
+        //提交事务
+        sqlSession.commit();
+        findAllTest();
+    }
+
+    //修改用户
+    @Test
+    public void updateUserTest(){
+        mapper.updateUser(new User(1,"王五","123456"));
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    //删除用户
+    @Test
+    public void deleteUserTest(){
+        mapper.deleteUser(1);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+
+
 }
